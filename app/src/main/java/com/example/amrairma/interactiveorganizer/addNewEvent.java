a843d34cd32f1da64171624c9c20c0d8fae37a29
Irma Karasoftic <irma.karasoftic@gmail.com>
2016-11-03 15:28:00 +0100

    added validation to home screen

--- app/src/main/java/com/example/amrairma/interactiveorganizer/addNewEvent.java
+++ app/src/main/java/com/example/amrairma/interactiveorganizer/addNewEvent.java
@@ -8,7 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class addNewEvent extends AppCompatActivity {
@@ -17,10 +19,17 @@ public class addNewEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_event);
+        TextView selectedDate=(TextView) findViewById(R.id.selDate);
+
+        long date=System.currentTimeMillis();
+        SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = sdf.format(date);
+        selectedDate.setText(dateString);
     }
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getFragmentManager(), "datePicker");
+
     }
 
 }
--- app/src/main/java/com/example/amrairma/interactiveorganizer/pocetniScreen.java
+++ app/src/main/java/com/example/amrairma/interactiveorganizer/pocetniScreen.java
@@ -3,11 +3,18 @@ package com.example.amrairma.interactiveorganizer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class pocetniScreen extends AppCompatActivity {
 
+    private static final Pattern sPattern
+            = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +22,25 @@ public class pocetniScreen extends AppCompatActivity {
         Button button= (Button) findViewById(R.id.buttonOK);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(pocetniScreen.this, HomePage.class);
-                startActivity(intent);
+
+                EditText firstName = (EditText) findViewById(R.id.firstName);
+                EditText lastName = (EditText) findViewById(R.id.lastName);
+                EditText email = (EditText) findViewById(R.id.email);
+                boolean isEmailValid= Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches();
+
+                if(firstName.getText().toString().length() != 0 && lastName.getText().toString().length()!=0 && isEmailValid) {
+                    Intent intent = new Intent(pocetniScreen.this, HomePage.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    if (isEmailValid)
+                    {
+                        Toast.makeText(pocetniScreen.this, "You need to enter your credentials.", Toast.LENGTH_SHORT).show();
+                    }
+                    Toast.makeText(pocetniScreen.this, "You need to enter valid email.", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
     }
