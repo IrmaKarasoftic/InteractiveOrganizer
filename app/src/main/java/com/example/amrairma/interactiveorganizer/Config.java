220843ae538d00384482403494cabfe33abe681e
Amra Hadziarapovic <ahadziarap1@etf.unsa.ba>
2016-11-13 21:46:20 +0100

    Radi dodavanje eventova

--- app/src/main/java/com/example/amrairma/interactiveorganizer/Config.java
+++ app/src/main/java/com/example/amrairma/interactiveorganizer/Config.java
@@ -11,12 +11,17 @@ public class Config {
     // Ovaj ip je samo kad lokalno koristis, jer je virutualna masina ne moze localhost
     // bio je addPerson.php, ali ste ga u php-u nazvali Register, pa eto :D
     public static final String URL_OK="http://10.0.2.2:8080/Register.php";
+    public static final String URL_ADD="http://10.0.2.2:8080/AddEvent.php";
 
     public static final String KEY_ID = "id";
     public static final String KEY_NAME = "name";
     public static final String KEY_LASTNAME = "lastname";
     public static final String KEY_EMAIL = "email";
     //public static final String KEY_DATE = "birth";
+    public static final String KEY_ENAME = "title";
+    public static final String KEY_ETYPE = "type";
+    public static final String KEY_EDESC = "description";
+    public static final String KEY_EDT = "dateAndTime";
 
     //JSON Tags
    // public static final String TAG_JSON_ARRAY="result";
--- app/src/main/java/com/example/amrairma/interactiveorganizer/addNewEvent.java
+++ app/src/main/java/com/example/amrairma/interactiveorganizer/addNewEvent.java
@@ -4,17 +4,41 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.DatabaseErrorHandler;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
+import static android.R.attr.button;
 
 public class addNewEvent extends AppCompatActivity {
 
+    private EditText dsc;
+    private EditText eventName;
+    private Spinner eventType;
+    private TimePicker eventTime;
+    private TextView eventDate;
+    private Button button;
+
+    SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +46,100 @@ public class addNewEvent extends AppCompatActivity {
         TextView selectedDate=(TextView) findViewById(R.id.selDate);
 
         long date=System.currentTimeMillis();
-        SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
-        String dateString = sdf.format(date);
+        //SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = sdf2.format(date);
         selectedDate.setText(dateString);
+        button= (Button) findViewById(R.id.buttonAdd);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                dsc= (EditText) findViewById(R.id.description);
+                eventName = (EditText) findViewById(R.id.title);
+                eventDate = (TextView) findViewById(R.id.selDate); //ovo poslije u date da se moze upisati u bazu
+                eventType = (Spinner) findViewById(R.id.spinner);
+                eventTime = (TimePicker) findViewById(R.id.tp_timepicker);
+                eventTime.setIs24HourView(true);
+                if(eventName.getText().toString().length() != 0 && eventType.getSelectedItem().toString().length()!=0) //ovdje jos provjeriti jel setovan time picker) {
+                    addEvent();
+
+                else
+                {
+                    Toast.makeText(addNewEvent.this, " Something is wrong.", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+
+    });
     }
+
+    public void addEvent(){
+
+        final String description = dsc.getText().toString().trim();
+        final String title=eventName.getText().toString().trim();
+        final String type=eventType.getSelectedItem().toString().trim();
+        final String datum=eventDate.getText().toString();
+        final int sati=eventTime.getCurrentHour();
+        final int min=eventTime.getCurrentMinute();
+        final String string=datum+" "+sati+":"+min+":00";
+        Date dateAndTime=null;
+        try{
+            dateAndTime=sdf.parse(string);
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+
+        final String finalDateAndTime = sdf.format(dateAndTime);
+        class AddEvent extends AsyncTask<Void,Void,String> {
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute(){
+                super.onPreExecute();
+                loading = ProgressDialog.show(addNewEvent.this,"Adding...","Wait..",false,false);
+
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(addNewEvent.this,s,Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(addNewEvent.this, HomePage.class);
+                startActivity(intent);
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String,String> params = new HashMap<>();
+                params.put(Config.KEY_ENAME,title);
+                params.put(Config.KEY_EDESC,description);
+                params.put(Config.KEY_EDT, finalDateAndTime);
+                params.put(Config.KEY_ETYPE,type);
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(Config.URL_ADD, params);
+                return res;
+            }
+
+        }
+
+        AddEvent ae= new AddEvent();
+        ae.execute();
+
+    }
+    // @Override
+    public void onClick(View v) {
+        if(v == button){
+            addEvent();
+        }
+
+    }
+
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getFragmentManager(), "datePicker");
